9469bbfc5445315eab9bf950071cdaadc79ed753
z <136988523+Freakoverse@users.noreply.github.com>
2026-02-16 00:37:32 +0200

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jumblednn/sw.js', { scope: '/jumblednn/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
